2e8e7b55894dc1cf35b84ba8ec732812e389a648
Oleg Khorev <o.g.khorev@gmail.com>
2023-06-14 15:53:26 +0300

    Example010_MethodArray

---
+++ lesson2 c#/Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 13, 22, 48, 4, 55, 61, 17, 48, 19 };
+
+int n = array.Length;
+int find = 48;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
